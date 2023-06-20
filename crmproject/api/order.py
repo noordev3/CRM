f9f6c896c70f83d5f813fa0054eaf239cc8b42c9
Karrar Emad <karrarprogrammer01@gmail.com>
2023-06-20 21:07:22 +0300

    order with items

--- crmproject/api/order.py
+++ crmproject/api/order.py
@@ -14,9 +14,14 @@ def order(request, id:int):
     return Order.objects.get(id = id)
 
 @order_router.get("/orderCheck", response=MessageOut)
-def order(request, user_id: int, cost: float, quantity: int):
+def order(request, user_id: int, cost: float, quantity: int, *args, **kwargs):
     user = User.objects.get(id = user_id)
     address, _ = Address.objects.get_or_create(name=user.city, user=user)
-    print(address, user)
-    print("order", Order.objects.create(address = address, cost = cost, user = user, quantity = quantity))
+    items_id = request.GET.getlist("item")
+    items_qty = request.GET.getlist("qty")
+
+    order = Order.objects.create(address = address, cost = cost, user = user, quantity = quantity)
+    products = Product.objects.filter(id__in = items_id)
+    for idx, product in enumerate(products):
+        Item.objects.create(order = order, product = product, quantity = items_qty[idx])
     return {"message": "order created successfully"}
